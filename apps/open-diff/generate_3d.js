ef1f26e85aeaaad44dda8b8aaf869b5b2e0ed201
Enrique Vela <enriquevela@alu.comillas.edu>
2026-08-18 01:24:01 +0200

    Deploy clean static website for GitHub Pages

---
+++ apps/open-diff/generate_3d.js
@@ -0,0 +1,7 @@
+// A helper script to verify logic before dropping into the html.
+// Here I'll author the 3d engine and geometry functions.
+const faces = [];
+function addFace(pts, fill, stroke) {
+  faces.push({ pts, fill, stroke });
+}
+//...
